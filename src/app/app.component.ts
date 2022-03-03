3fa2390d19dde45bc9fcd958fb008d2e68b51dc9
_manalelsayed <manal.afifi626@gmail.com>
2022-03-03 04:34:07 +0200

    6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
-import {Component, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'myfirstapp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'day1';
-}
+
   
+export class AppComponent implements  AfterViewInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  title = 'day1';
+  constructor(){}
+  @ViewChild(ProductsComponent) c!: ProductsComponent;
+
+  ngAfterViewInit() {
+    this.c.renderValues();
+  }  
+
+
+   
+}
+function ChildComponent(ChildComponent: any) {
+  throw new Error('Function not implemented.');
+}
+
